f5aa48e3ce9c972d5db74543a0cb911e1ab5bfe5
SeekerOfTheGoldenPeach <oktawian512@gmail.com>
2024-01-31 18:37:07 +0100

    Movig draw functionality to main
    Movig draw to main as there is no way
    to pass coordiantes to display func

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,54 @@
 #include "../include/calculate_inertia.hpp"
 #include "../include/read_coordinates.hpp"
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <math.h>
+#include <vector> 
+
+void draw(std::vector<Coordinates> figure)
+{
+    glBegin(GL_LINE_STRIP);
+    for (size_t i = 0; i<figure.size(); i++)
+        {   
+            glVertex2f(figure.at(i).x,figure.at(i).y);
+        }
+    glEnd();
+    glutSwapBuffers();
+}
+
+void reshape(int width, int height)
+{
+    glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+    glClear(GL_COLOR_BUFFER_BIT);
+    gluOrtho2D(-500.0, 500.0, -500.0, 500.0);
+    glMatrixMode ( GL_PROJECTION );
+    glLoadIdentity();
+    glutSwapBuffers();
+    }
+
+void display()
+{   glMatrixMode ( GL_PROJECTION );
+    glClear(GL_COLOR_BUFFER_BIT);
+    gluOrtho2D(-500.0, 500.0, -500.0, 500.0);
+    glLoadIdentity();
+    std::vector<Coordinates> figureCoordinates = getCoordinatesFromFile("../data/figure_1.txt");
+    draw(figureCoordinates);
+}
 
 
-int main()
+void keybord(unsigned char button, int x, int y)
+{
+    switch (button)
+
+    {
+    case 27:
+        exit(0);
+    }
+}
+
+
+    int main(int argc, char **argv)
 {
     std::vector<Coordinates> figureCoordinates = getCoordinatesFromFile("../data/figure_1.txt");
 
@@ -16,5 +62,16 @@ int main()
     std::cout << "figure iy: " << figure.iy << '\n';
     std::cout << "figure ixy: " << figure.ixy << '\n';
 
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_SINGLE | GLUT_RGBA);
+    glutInitWindowSize(500,500); /* 500 x 500 pixel window */
+    glutInitWindowPosition(0,0);    
+    glutCreateWindow("OpenGL Test");
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keybord);
+    glClearColor(0,0,0,1);
+    glutMainLoop();
+    
     return 0;
 }
